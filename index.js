0119a79568b41fcc0fa4a6ae754ae79572d075ad
Md. Sakhawat Hossain <biplobbms@gmail.com>
2022-06-05 15:18:12 +0600

    Booking Done

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express();
+
+// MiddleWare
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const port = 5000;
 
-// MongoDB
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri =
-	'mongodb+srv://burjdb:Biplob@@db1@cluster0.lgf1sij.mongodb.net/?retryWrites=true&w=majority';
+	'mongodb+srv://burjdb:Biplobbookingsdb1@cluster0.lgf1sij.mongodb.net/?retryWrites=true&w=majority';
 const client = new MongoClient(uri, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	serverApi: ServerApiVersion.v1,
 });
 client.connect((err) => {
-	const collection = client.db('test').collection('devices');
-	// perform actions on the collection object
-	client.close();
+	const bookingsCollection = client.db('burjdb').collection('bookings');
+	console.log('DB Connection Success');
+	app.post('/addBooking', (req, res) => {
+		const newBooking = req.body;
+		bookingsCollection.insertOne(newBooking).then((result) => {
+			res.send(result.insertedId);
+		});
+		console.log(newBooking);
+	});
 });
 
 app.get('/', (req, res) => {
